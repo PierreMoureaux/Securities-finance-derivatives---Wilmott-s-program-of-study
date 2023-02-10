78a4a123fc043b44b6f766e5c9344898a3124019
Pierre Moureaux <124352709+PierreMoureaux@users.noreply.github.com>
2023-02-10 08:40:48 +0100

    Add files via upload

--- I - Explicit method for vanilla TRS/BulletTRS_noIncome.py
+++ I - Explicit method for vanilla TRS/BulletTRS_noIncome.py
@@ -8,13 +8,14 @@ attributes and functions of FD
 class FiniteDifferences(object):
 
     def __init__(
-        self, S0, K, r=0.05,r0=0.05, T=1, 
+        self, S0, K, r=0.05,rTRS=0.05,cyield=0, T=1, 
         Smax=1, M=1, N=1, is_pay_perf=False
     ):
         self.S0 = S0
         self.K = K
         self.r = r
-        self.r0 = r0
+        self.rTRS = rTRS
+        self.cyield = cyield
         self.T = T
         self.Smax = Smax
         self.M, self.N = M, N
@@ -32,6 +33,21 @@ class FiniteDifferences(object):
     @property
     def dt(self):
         return self.T/float(self.N)
+    
+    @abstractmethod
+    def setup_boundary_conditions(self):
+        raise NotImplementedError('Implementation required!')
+
+    @abstractmethod
+    def setup_coefficients(self):
+        raise NotImplementedError('Implementation required!')
+
+    @abstractmethod
+    def traverse_grid(self):
+        """  
+        Iterate the grid backwards in time 
+        """
+        raise NotImplementedError('Implementation required!')
 
     @abstractmethod
     def interpolate(self):
@@ -55,26 +71,22 @@ class FDExplicitEu(FiniteDifferences):
 
     def setup_boundary_conditions(self):
         if self.is_receive_perf:
-            self.grid[:,-1] = self.boundary_conds - self.K - self.r0*self.K*self.T
-            self.grid[-1,:-1] = (self.Smax-self.K - self.r0*self.K*self.T) * \
+            self.grid[:,-1] = self.boundary_conds - self.K - self.rTRS*self.K*self.T
+            self.grid[-1,:-1] = (self.Smax-self.K - self.rTRS*self.K*self.T) * \
                 np.exp(-self.r*self.dt*(self.N-self.j_values))
         else:
-            self.grid[:,-1] = -(self.boundary_conds - self.K - self.r0*self.K*self.T)
-            self.grid[0,:-1] = (self.K-self.Smax + self.r0*self.K*self.T) * \
+            self.grid[:,-1] = -(self.boundary_conds - self.K - self.rTRS*self.K*self.T)
+            self.grid[0,:-1] = (self.K-self.Smax + self.rTRS*self.K*self.T) * \
                 np.exp(-self.r*self.dt*(self.N-self.j_values))
 
     def setup_coefficients(self):
-        self.a = 0.5*self.dt*self.r*self.i_values
+        self.a = self.dt*(self.r -self.cyield)*self.i_values
         self.b = 1 - self.dt*self.r
-        self.c = 0.5*self.dt*self.r*self.i_values
 
     def traverse_grid(self):
         for j in reversed(self.j_values):
             for i in range(self.M)[2:]:
-                self.grid[i,j] = \
-                    self.a[i]*self.grid[i-1,j+1] +\
-                    self.b*self.grid[i,j+1] + \
-                    self.c[i]*self.grid[i+1,j+1]
+                self.grid[i,j] = self.a[i]+self.b*self.grid[i,j+1]
 
-TRS = FDExplicitEu(50, 50, r=0.1, r0=0.04, T=5./12., Smax=100, M=100, N=1000, is_pay_perf=True)
+TRS = FDExplicitEu(50, 50, r=0.1, rTRS=0.04,cyield=0.02, T=5./12., Smax=100, M=100, N=1000, is_pay_perf=True)
 print(TRS.price())
